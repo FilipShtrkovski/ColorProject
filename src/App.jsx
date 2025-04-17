3c2c67171a7bf1f28a39c1b0427bfccd626dc9d8
Filip <filip.strkovski13@gmail.com>
2025-04-17 19:44:25 +0200

    starting color project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Component } from 'react'
+import './App.css'
+
+class App extends Component {
+  render(){
+    return (
+        <h1>Colors</h1>
+      )
+  }
+  
+}
+
+export default App
