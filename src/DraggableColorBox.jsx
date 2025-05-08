6f770e0e7ce116c8cf87cd9a42342e38a315beea
Filip <filip.strkovski13@gmail.com>
2025-05-08 17:01:28 +0200

    style DraggableColorBox

--- src/DraggableColorBox.jsx
+++ src/DraggableColorBox.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { withStyles } from '@mui/styles'
+import { FaTrashCan } from "react-icons/fa6";
 
 const styles = {
-    root:{
+    root: {
         width: '20%',
         height: '25%',
         margin: '0 auto',
@@ -10,15 +11,39 @@ const styles = {
         position: 'relative',
         cursor: 'pointer',
         marginBottom: '-7px',
-        
+        '&:hover svg': {
+          color: 'white',
+          transform: 'scale(2.0)'
+        } 
+    },
+    boxContent: {
+      display: 'flex',
+      position: 'absolute',
+      width: '100%',
+      left: '0px',
+      color: 'rgba(0,0,0,0.5)',
+      bottom: '0px',
+      padding: '10px',
+      letterSpacing: '1px',
+      textTransform: 'uppercase',
+      fontSize: '12px',
+      justifyContent: 'space-between'
+    },
+    deleteIcon: {
+      transition: 'all 0.3s ease-in-out'
     }
     
 }
 
 function DraggableColorBox(props) {
+  const {classes} = props
   return (
-    <div className={props.classes.root} style={{backgroundColor:props.color}}>
-        {props.name}
+    <div className={classes.root} style={{backgroundColor:props.color}}>
+      <div className={classes.boxContent}>
+        <span>{props.name}</span>
+        <FaTrashCan className={classes.deleteIcon}/>
+      </div>
+        
     </div>
   )
 }
